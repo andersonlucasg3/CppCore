6be064e70f5a23ee6ea9c33da962da28ededfbdf
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2025-08-16 01:30:36 -0300

    Fixed executable linkage for Android NDK

--- Core/Modules/CoreTests/Sources/main.cpp
+++ Core/Modules/CoreTests/Sources/main.cpp
@@ -8,3 +8,6 @@ static void CustomMain(int argc, char* argv[])
 }
 
 DEFINE_TEST_RUNNER(CustomMain(argc, argv));
+
+// do not remove, faking pthread_atfork
+int pthread_atfork(void (* _Nullable)(), void (* _Nullable)(), void (* _Nullable)()) {}
